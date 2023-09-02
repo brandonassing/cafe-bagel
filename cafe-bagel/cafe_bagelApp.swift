577396bb2c36ff86e9e4fa144c031a76bb428fd6
Brandodn Assing <assing.brandon@gmail.com>
2023-09-01 23:27:58 -0400

    Initial project

---
+++ cafe-bagel/cafe_bagelApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafe_bagelApp.swift
+//  cafe-bagel
+//
+//  Created by Brandon Assing on 2023-09-01.
+//
+
+import SwiftUI
+
+@main
+struct cafe_bagelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
