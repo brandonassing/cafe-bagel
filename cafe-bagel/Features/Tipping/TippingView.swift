01842bc84114114a8ddba0647f0899d17acbfc76
Brandodn Assing <assing.brandon@gmail.com>
2023-09-04 14:04:49 -0400

    Added Duolingo link to locale button

--- cafe-bagel/Features/Tipping/TippingView.swift
+++ cafe-bagel/Features/Tipping/TippingView.swift
@@ -46,7 +46,10 @@ struct TippingView: View {
 			Spacer()
 			
 			Button {
-				print("Locale")
+				// TODO: replace with SKStoreProductViewController to open app store in modal
+				if let url = URL(string: "https://apps.apple.com/ca/app/duolingo-language-lessons/id570060128") {
+					UIApplication.shared.open(url)
+				}
 			} label: {
 				Label(self.viewModel.locale, systemImage: "globe")
 					.foregroundColor(StyleGuide.TextStyle.inlineButton.color)
