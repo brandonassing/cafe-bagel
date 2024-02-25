1fe3457fd37eef76fe906c8a025de149e2645544
Brandodn Assing <assing.brandon@gmail.com>
2024-02-24 22:55:45 -0500

    Added "Place order" button that triggers tipping view Updated tipping flow to dismiss and return to cart builder after post payment "New sale" tapped

--- cafe-bagel/Features/CartBuilding/CartBuildingViewModel.swift
+++ cafe-bagel/Features/CartBuilding/CartBuildingViewModel.swift
@@ -4,14 +4,11 @@ import Combine
 class CartBuildingViewModel: ObservableObject {
 	let randomizePreTipAmount: PassthroughSubject<Void, Never>
 	
-	@Published var checkout: Checkout
+	@Published var checkout: Checkout?
 
 	init() {
 		// MARK: Inputs
 		self.randomizePreTipAmount = PassthroughSubject<Void, Never>()
-
-		// MARK: Output defaults
-		self.checkout = Checkout(preTipAmount: Money.random())
 		
 		// MARK: Functionality
 		self.randomizePreTipAmount
